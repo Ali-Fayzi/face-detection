fe13a9156a5f7b4ee83c3b119b69d84af1808073
Ali-Fayzi <a.fayzi@outlook.com>
2024-01-23 10:33:14 +0330

    Create Yolo Face Detection & Test Result

--- face_detection/detections.py
+++ face_detection/detections.py
@@ -1,21 +1,42 @@
 import cv2
 from matplotlib import pyplot as plt  
 from detections_models.opencv.face_detection import Opencv_Face_Detection
-
+from detections_models.yolov8_face.face_detection import Yolo_Face_Detection
 
 
 
 if __name__ == "__main__":
+
     image_path     = r"D:\Personal_Project\Github_Project\face-recognition-repo\test_images\1.png"
     image          = cv2.imread(image_path)
     image          = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     # create opencv face detection instance
-    face_detection = Opencv_Face_Detection()
-    image, bboxes, crops = face_detection.detect(image=image,return_crops=True,draw_bbox=True)
-    for box in bboxes:
-        cv2.rectangle(image , (box[0],box[1]),(box[2],box[3]),(0,0,255),0)
-    plt.imshow(image,cmap='gray')
-    plt.show()
-    for crop in crops:
-        plt.imshow(crop,cmap='gray')
+    face_detection_model = "yolo"#opencv,yolo
+    
+    if face_detection_model == "opencv":
+        face_detection = Opencv_Face_Detection()
+        image, bboxes, crops = face_detection.detect(image=image,return_crops=True,draw_bbox=True)
+        for box in bboxes:
+            cv2.rectangle(image , (box[0],box[1]),(box[2],box[3]),(0,0,255),0)
+        plt.imshow(image,cmap='gray')
         plt.show()
+        for crop in crops:
+            plt.imshow(crop,cmap='gray')
+            plt.show()  
+
+    elif face_detection_model == "yolo":
+        face_detection = Yolo_Face_Detection()
+        image, bboxes, keypoints, crops  = face_detection.detect(image=image,return_crops=True,return_keypoints=False,draw_bbox=True,draw_keypoint=True)
+        for keypoint in keypoints:
+            for key in keypoint:
+                center_coordinates = (key[0], key[1]) 
+                radius = 2
+                color = (255, 0, 0) 
+                thickness = -1
+                image = cv2.circle(image, center_coordinates, radius, color, thickness) 
+        plt.imshow(image,cmap='gray')
+        plt.show()
+        for crop in crops:
+            plt.imshow(crop,cmap='gray')
+            plt.show()  
+      
---
+++ face_detection/detections_models/yolov8_face/face_detection.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np 
+from ultralytics import YOLO
+
+class Yolo_Face_Detection:
+    def __init__(self):
+        model_weight = "./face_detection/detections_models/yolov8_face/weight/yolov8n-face.pt"
+        self.model   = YOLO(model_weight) 
+        self.warmup()
+
+    def warmup(self):
+        input = np.ones((640,640,3))
+        self.model(input,verbose=False,conf=0.7)
+        print("Yolo Model Warmup Is Done!")
+    def detect(self, image, return_crops=False, return_keypoints=False, draw_bbox=False,draw_keypoint=False):
+        assert image is not None,"Image is None!"
+        bboxes      = []
+        crops       = []
+        keypoints   = []
+        image_copy = image.copy() if return_crops else None
+        results = self.model(image,verbose=False,conf=0.7)
+        for result in results:
+            boxes = result.boxes
+            keys = result.keypoints
+            for idx,(box , keypoint) in enumerate(zip(boxes.xyxy.tolist() , keys.xy.tolist())):
+                box      = [int(item) for item in box]
+                keypoint = [ (int(item[0]),int(item[1])) for item in keypoint]
+                x1, y1, x2, y2 = box
+
+                bboxes.append([x1,y1,x2,y2])
+                if return_keypoints:
+                    keypoints.append(keypoint)
+                if return_crops:
+                    crop    = image_copy[y1:y2 , x1:x2]
+                    crops.append(crop)
+                if draw_bbox:
+                    start_point = (x1, y1)
+                    end_point   = (x2, y2)
+                    color       = (0, 0, 255)
+                    thickness   = 2
+                    image       = cv2.rectangle(image, start_point, end_point, color, thickness)
+                if draw_keypoint:
+                    for key in keypoint:
+                        center_coordinates = (key[0], key[1]) 
+                        radius = 2
+                        color = (255, 0, 0) 
+                        thickness = -1
+                        image = cv2.circle(image, center_coordinates, radius, color, thickness) 
+        return image, bboxes, keypoints, crops 
