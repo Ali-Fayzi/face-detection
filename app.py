3d1b0362ac47e902279ec228b2a661c5f6b016a7
Ali-Fayzi <a.fayzi@outlook.com>
2024-01-23 14:01:35 +0330

    Cereate Simple Face Alignment with left-eye , right-eye , euclidean distance

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ warnings.filterwarnings("ignore")
 import cv2 
 from matplotlib import pyplot as plt 
 from face_detection.detections import Face_Detection
-
+from face_alignment.simple_alignment.align import Simple_Alignment
 if __name__ == "__main__":
     print("Face Detection Model")
 
-    image_path     = r"./test_images/1.png"
+    image_path     = r"./test_images/3.png"
     image          = cv2.imread(image_path)
     image          = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     # create opencv face detection instance
@@ -26,4 +26,16 @@ if __name__ == "__main__":
     plt.imshow(image,cmap='gray')
     plt.show()
 
+    if face_detection_models != "opencv":
+        face_alignment = Simple_Alignment()
+        for face , keypoint in zip(crops,keypoints):
+            main_face , aligned_face = face_alignment.align(face,keypoint)
+
+            plt.subplot(1,2,1)
+            plt.imshow(face,cmap='gray')
+
+            plt.subplot(1,2,2)
+            plt.imshow(aligned_face,cmap='gray')
+
+            plt.show()
 
