4a5cc086be6338959c9bcdbcedaa26e9c6b9a992
Ali-Fayzi <a.fayzi@outlook.com>
2024-01-23 10:56:27 +0330

    Create Retina Face Detector & Test it

--- face_detection/detections.py
+++ face_detection/detections.py
@@ -1,8 +1,10 @@
+import warnings
+warnings.filterwarnings("ignore")
 import cv2
 from matplotlib import pyplot as plt  
 from detections_models.opencv.face_detection import Opencv_Face_Detection
 from detections_models.yolov8_face.face_detection import Yolo_Face_Detection
-
+from detections_models.retinaface.face_detection import Retina_Face_Detection
 
 
 if __name__ == "__main__":
@@ -11,7 +13,7 @@ if __name__ == "__main__":
     image          = cv2.imread(image_path)
     image          = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     # create opencv face detection instance
-    face_detection_model = "yolo"#opencv,yolo
+    face_detection_model = "retinaface"#opencv,yolo,retinaface
     
     if face_detection_model == "opencv":
         face_detection = Opencv_Face_Detection()
@@ -26,6 +28,21 @@ if __name__ == "__main__":
 
     elif face_detection_model == "yolo":
         face_detection = Yolo_Face_Detection()
+        image, bboxes, keypoints, crops  = face_detection.detect(image=image,return_crops=True,return_keypoints=False,draw_bbox=True,draw_keypoint=True)
+        for keypoint in keypoints:
+            for key in keypoint:
+                center_coordinates = (key[0], key[1]) 
+                radius = 2
+                color = (255, 0, 0) 
+                thickness = -1
+                image = cv2.circle(image, center_coordinates, radius, color, thickness) 
+        plt.imshow(image,cmap='gray')
+        plt.show()
+        for crop in crops:
+            plt.imshow(crop,cmap='gray')
+            plt.show()  
+    elif face_detection_model == "retinaface":
+        face_detection = Retina_Face_Detection()
         image, bboxes, keypoints, crops  = face_detection.detect(image=image,return_crops=True,return_keypoints=False,draw_bbox=True,draw_keypoint=True)
         for keypoint in keypoints:
             for key in keypoint:
---
+++ face_detection/detections_models/retinaface/face_detection.py
@@ -0,0 +1,44 @@
+import cv2
+from retinaface.pre_trained_models import get_model
+
+
+class Retina_Face_Detection:
+    def __init__(self):
+        self.model = get_model("resnet50_2020-07-20", max_size=2048)
+        self.model.eval()
+
+    def detect(self, image, return_crops=False, return_keypoints=False, draw_bbox=False,draw_keypoint=False):
+        
+        bboxes      = []
+        crops       = []
+        keypoints   = []
+        image_copy = image.copy() if return_crops else None
+
+        output = self.model.predict_jsons(image)
+
+        for out in output:
+            x1,y1,x2,y2 = int(out['bbox'][0]),int(out['bbox'][1]),int(out['bbox'][2]),int(out['bbox'][3])
+            keypoint    = out['landmarks']
+            keypoint    = [ (int(item[0]),int(item[1])) for item in keypoint]
+
+            bboxes.append([x1,y1,x2,y2])
+            if return_keypoints:
+                keypoints.append(keypoint)
+            if return_crops:
+                crop    = image_copy[y1:y2 , x1:x2]
+                crops.append(crop)
+            if draw_bbox:
+                start_point = (x1, y1)
+                end_point   = (x2, y2)
+                color       = (0, 0, 255)
+                thickness   = 2
+                image       = cv2.rectangle(image, start_point, end_point, color, thickness)
+            if draw_keypoint:
+                for key in keypoint:
+                    center_coordinates = (key[0], key[1]) 
+                    radius = 2
+                    color = (255, 0, 0) 
+                    thickness = -1
+                    image = cv2.circle(image, center_coordinates, radius, color, thickness) 
+
+        return image, bboxes, keypoints, crops 
